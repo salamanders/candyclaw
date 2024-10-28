76eeeec140bb12ba43c4b74209a1605031cc488a
Benjamin Hill <benjaminhill@gmail.com>
2024-10-27 17:28:36 -0700

    Service

--- candyclaw_boot_script.sh
+++ candyclaw_boot_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# copy this to /etc/rc.local/ then `sudo chmod +x /etc/rc.local``
+# Option 1: copy this to /etc/rc.local/ then `sudo chmod +x /etc/rc.local``
 
 # Start your Python script in the background and ignore HUP signals
 nohup /usr/bin/python3 /home/pi/candyclaw/app.py &
