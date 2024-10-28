318217fe83d9ee67a8caed522a484a1206fe89c4
Benjamin Hill <benjaminhill@gmail.com>
2024-10-27 17:23:16 -0700

    LAN and run on start

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def handle_joystick_input():
   return 'OK'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
---
+++ candyclaw_boot_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# copy this to /etc/rc.local/ then `sudo chmod +x /etc/rc.local``
+
+# Start your Python script in the background and ignore HUP signals
+nohup /usr/bin/python3 /home/pi/candyclaw/app.py &
+
+exit 0
